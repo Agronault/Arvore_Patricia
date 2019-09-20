e1285cf5c7889ac5b3c8af20ceb236b147db6a63
carlosivis <carlosivis1@gmail.com>
2019-09-20 11:44:40 -0300

    Revert "V0.3"
    This reverts commit e3dfce6f274cd22c2a8a2751147a719c27a8e3b8.

--- src/lista5/ArvorePatricia.java
+++ src/lista5/ArvorePatricia.java
@@ -9,14 +9,6 @@ public class ArvorePatricia {
     private NoArvorePatricia raiz;
     private final int numeroDeBitsNaChave;
     private int nosVisitadosPesquisa;
-
-    public int getNosVisitadosPesquisa() {
-        return nosVisitadosPesquisa;
-    }
-
-    public int getCaracteresDistintos() {
-        return caracteresDistintos;
-    }
     private int caracteresDistintos;
 
     public ArvorePatricia(int nbitsChave) {
@@ -94,25 +86,12 @@ public class ArvorePatricia {
             else
                 aux=((NoInternoArvorePatricia)aux).filhoEsquerda;
         }
-        //NoArvorePatricia interno= criaNoInterno(nosVisitadosPesquisa, aux, raiz);
-        return insereEntre(chaveInsercao, aux, ((NoInternoArvorePatricia)aux).indice);
+        //TODO
         }
     }
     
     private NoArvorePatricia insereEntre(char chaveInsercao, NoArvorePatricia raizAtual,int posicaoAtual){
-        NoInternoArvorePatricia aux=null;
-        if(!verificaNoExterno(raizAtual)) aux= ((NoInternoArvorePatricia)raizAtual);
-        if(verificaNoExterno(raizAtual)|| posicaoAtual< aux.indice){
-        NoArvorePatricia replace1 = criaNoExterno(chaveInsercao);
-        if(testaBit(posicaoAtual, chaveInsercao)==1) return criaNoInterno(posicaoAtual, raizAtual, replace1);
-        return criaNoInterno(posicaoAtual, replace1, raizAtual);
-        }else{
-        if(testaBit(posicaoAtual, chaveInsercao)==1)
-            aux.filhoDireita=insereEntre(chaveInsercao, aux.filhoDireita, posicaoAtual);
-        else
-            aux.filhoEsquerda=insereEntre(chaveInsercao, aux.filhoEsquerda, posicaoAtual);
-        return aux;
-            }
+    //TODO
     }
 
     private boolean verificaNoExterno(NoArvorePatricia noAtual) {
@@ -135,10 +114,6 @@ public class ArvorePatricia {
     aux.chave=chaveInsercao;
     aux.quantidade=1;
     return aux;
-    }
-    
-    public void insere(char chaveInsercao){
-    this.insere(chaveInsercao, raiz);
     }
 
 }
--- src/lista5/Lista5.java
+++ src/lista5/Lista5.java
@@ -4,19 +4,7 @@ package lista5;
 public class Lista5 {
 
     public static void main(String[] args) {
-        ArvorePatricia[] arvores = new ArvorePatricia[8];
-        
-        
-        for(int i=0; i<arvores.length; i++){
-            arvores[i]= new ArvorePatricia(i+1);
-            
-            
-            
-            
-            
-        }
-        
-    
+        ArvorePatricia arvore = new ArvorePatricia(4);
     }
     
 }
